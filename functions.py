1ce8bac07a950707b70b03b347b06f72342cf4a6
Gediminas Šimavičius <gediminas.simavicius@gmail.com>
2021-09-05 15:09:24 +0300

    alpha_selector function

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import Ridge, Lasso
 from sklearn.model_selection import cross_val_score
 
 def unbalanced_features(table: pd.DataFrame) -> pd.DataFrame:
@@ -22,12 +24,15 @@ def feature_selector(eli5_df: pd.DataFrame, baseline_score: float, model: object
     finds the lowest score and returns dataframe with selected features.
     
     Keyword arguments:
-    eli5_df -- eli5 dataframe with weight of features
-    baseline_score -- score 
-    start -- start point of the iteration (default 1)
-    end -- end point of the iteration (default 11)
-    step -- step of the iteration (default 1) 
-    divider -- transforms int to float (default 10000)
+    eli5_df -- eli5 dataframe with weight of features.
+    baseline_score -- score for comparison.
+    model -- sklearn model.
+    train_evaulation_df -- dataframe used for cross-validation.
+    target -- target feature name.
+    start -- start point of the iteration (default 1).
+    end -- end point of the iteration (default 11).
+    step -- step of the iteration (default 1). 
+    divider -- transforms int to float (default 10000).
     
     Whereas Python range() function works only with integers divider is used to transform
     integers to float numbers with required decimal places. 
@@ -66,4 +71,43 @@ def feature_selector(eli5_df: pd.DataFrame, baseline_score: float, model: object
     else:
         print("The score was not improved.")
         print("The lowest achieved score:", round(min(avg_scores), 5))
-        return final_selected_features 
+        return final_selected_features
+    
+    
+def alpha_selector(model_name: str, X_eval: pd.DataFrame, y_eval: pd.Series) -> float:
+    """Takes in a sklearn model name and iterates through sequence from 0.1 to 10 with step 0.1,
+    where each number each time is assigned to Alpha attribute.
+    
+    Keyword arguments:
+    model_name -- sklearn model name: Ridge or Lasso.
+    X_eval -- variables as a dataframe which are used in cross-validation.
+    y_eval -- target as a series which are used in cross-validation.
+    """
+     
+    alpha_sequence = np.arange(0.1, 10.1, 0.1)
+    lowest_score = 1
+    final_alpha = 0
+
+    if model_name == "Ridge":
+        for alpha in alpha_sequence:
+            regression = Ridge(alpha=alpha)
+            scores = cross_val_score(regression, X_eval, y_eval, scoring="neg_root_mean_squared_error")
+            avg_score = round(abs(scores.mean()), 5)
+            if avg_score < lowest_score:
+                lowest_score = avg_score
+                final_alpha = alpha
+        print("The lowest reached score:", lowest_score)
+        print("Optimal alpha:", final_alpha)
+    
+    elif model_name == "Lasso":
+        for alpha in alpha_sequence:
+            regression = Lasso(alpha=alpha)
+            scores = cross_val_score(regression, X_eval, y_eval, scoring="neg_root_mean_squared_error")
+            avg_score = round(abs(scores.mean()), 5)
+            if avg_score < lowest_score:
+                lowest_score = avg_score
+                final_alpha = alpha
+        print("The lowest reached score:", lowest_score)
+        print("Optimal alpha:", final_alpha)
+        
+    return final_alpha
